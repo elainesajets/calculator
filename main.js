bdff76bae30313946771ac0fa9fa01a78f630802
Elaine Sajets <elainesajets@gmail.com>
2025-03-12 20:31:27 +0900

    Buttons functional But not able to use operate() yet. I have to figure out how to store the second number.

--- main.js
+++ main.js
@@ -14,6 +14,9 @@ function divide(a, b) {
   return b === 0 ? 'Division by zero' : a / b;
 }
 
+function modulo(a, b) {
+  return a % b;
+}
 let num1, num2, operator;
 
 function operate(number1, number2, operatorChoice) {
@@ -22,11 +25,45 @@ function operate(number1, number2, operatorChoice) {
       return add(number1, number2);
     case '-':
       return subtract(number1, number2);
-    case '*':
+    case '*' || 'x':
       return multiply(number1, number2);
-    case '/':
+    case '/' || '÷':
       return divide(number1, number2);
+    case '%':
+      return modulo(number1, number2);
     default:
       return 'Invalid operator';
   }
 }
+
+const display = document.getElementById('display');
+const numberButtons = document.getElementById('numbers');
+const buttonContainer = document.getElementById('button-container');
+
+numberButtons.addEventListener('click', (e) => {
+  const button = e.target.closest('.number-button');
+  if (!button) return;
+  if (display.textContent.includes('.') && button.textContent === '.') return;
+
+  display.textContent += button.textContent;
+});
+
+buttonContainer.addEventListener('click', (e) => {
+  const button = e.target.closest('.top-button');
+  if (!button) return;
+  console.log(button.textContent);
+  if (button.textContent === 'AC') display.textContent = '';
+});
+
+buttonContainer.addEventListener('click', (e) => {
+  const button = e.target.closest('.operator-button');
+  if (!button) return;
+  if (button.textContent === '=') {
+    display.textContent = operate(num1, num2, operator);
+  } else {
+    if (!num1) num1 = display.textContent;
+    operator = button.textContent;
+    display.textContent = operator;
+    console.log(`${num1} ${operator}`);
+  }
+});
