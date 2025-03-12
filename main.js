ae9ba336d13f90c791c2d51cd065b6463b620446
Elaine Sajets <elainesajets@gmail.com>
2025-03-12 18:39:03 +0900

    Updated to IU. Added buttons, but no styling.

--- main.js
+++ main.js
@@ -11,5 +11,22 @@ function multiply(a, b) {
 }
 
 function divide(a, b) {
-  return a / b;
+  return b === 0 ? 'Division by zero' : a / b;
+}
+
+let num1, num2, operator;
+
+function operate(number1, number2, operatorChoice) {
+  switch (operatorChoice) {
+    case '+':
+      return add(number1, number2);
+    case '-':
+      return subtract(number1, number2);
+    case '*':
+      return multiply(number1, number2);
+    case '/':
+      return divide(number1, number2);
+    default:
+      return 'Invalid operator';
+  }
 }
