4a70b469e35c2b0cc098c861dd21469e2e7f7728
Elaine Sajets <elainesajets@gmail.com>
2025-03-17 23:21:23 +0900

    Added keyboard support Works somewhat, but is still buggy. Subtract doesn't work. Weird behaviour too when operator buttons are pressed more than once in a row.

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
+let num1 = 0,
+  num2 = 0,
+  operator = '',
+  sum = 0,
+  clearOnNextInput = false;
+calculationComplete = false; //True if next input is a new number
+
 function add(a, b) {
   return a + b;
 }
@@ -23,7 +30,8 @@ function noInput() {
 }
 
 function clearAll() {
-  display.textContent = '';
+  mainDisplay.textContent = '';
+  history.textContent = '';
   num1 = 0;
   num2 = 0;
   operator = '';
@@ -36,12 +44,14 @@ function clearAll() {
   console.log(num1, num2, operator);
 }
 
+//Removes highlight (.button-selected) from previously highlighted operator
 function deselectOperators() {
   document
     .querySelectorAll('.operator-button')
     .forEach((btn) => btn.classList.remove('button-selected'));
 }
 
+//For debugging
 function checkAll() {
   console.table({ num1, num2, operator, sum, clearOnNextInput });
 }
@@ -67,33 +77,35 @@ function operate(number1, number2, operatorChoice) {
   }
 }
 
-let num1 = 0;
-let num2 = 0;
-let operator = '';
-let sum = 0;
-let clearOnNextInput = false;
 //let operatorArr = ['+', '-', '*', 'x', '/', '÷', '%'];
 //let calculationArr = [0, '', 0];
 
-const display = document.getElementById('display');
+const mainDisplay = document.getElementById('main-display');
 const buttonContainer = document.getElementById('button-container');
+const history = document.getElementById('history-display');
+
+//Mouse event listeners
 
 buttonContainer.addEventListener('click', (e) => {
   const number = e.target.closest('.number-button');
   if (!number) return;
-  if (noInput() === true && display.textContent === '0') {
-    display.textContent = number.textContent;
+  if (noInput() === true && mainDisplay.textContent === '0') {
+    mainDisplay.textContent = number.textContent;
+    history.textContent = number.textContent;
     return;
   }
-  if (display.textContent.includes('.') && number.textContent === '.') return;
+  if (mainDisplay.textContent.includes('.') && number.textContent === '.')
+    return;
   if (clearOnNextInput) {
-    display.textContent = '';
+    mainDisplay.textContent = '';
     clearOnNextInput = false;
+    calculationComplete = false;
   }
   if (sum && num2) {
     clearAll();
   }
-  display.textContent += number.textContent;
+  mainDisplay.textContent += number.textContent;
+  history.textContent += number.textContent;
 });
 
 buttonContainer.addEventListener('click', (e) => {
@@ -109,32 +121,91 @@ buttonContainer.addEventListener('click', (e) => {
 buttonContainer.addEventListener('click', (e) => {
   const button = e.target.closest('.operator-button');
   if (!button) return;
-  if (!num1) num1 = Number(display.textContent);
-  if (!num2) num2 = Number(display.textContent);
+
+  if (!num1) num1 = Number(mainDisplay.textContent);
+  if (!num2) num2 = Number(mainDisplay.textContent);
 
   //Check if operator button has been highlighted and behave accordingly
   deselectOperators();
   button.classList.toggle('button-selected');
 
+  if (
+    operator &&
+    button.textContent !== '=' &&
+    !history.textContent.includes('=')
+  ) {
+    return;
+  } else if (operator && button.textContent !== '=') {
+    return;
+  }
+
+  if (calculationComplete && history.textContent.includes('=')) {
+    history.textContent = `${num1} ${button.textContent} `;
+    calculationComplete = false;
+  } else if (history.textContent.includes('=') && button.textContent === '=') {
+    return;
+  } else {
+    history.textContent += ` ${button.textContent} `;
+  }
+
   if (button.textContent === '=') {
     if (!num1 || !num2 || !operator) {
-      console.log('<Missing input');
+      console.log('Missing input');
       return;
     }
 
     checkAll(); //Debugging
     sum = operate(num1, num2, operator);
-    display.textContent = sum.toFixed(8);
+    mainDisplay.textContent = sum.toFixed(8) / 1;
     num1 = sum;
+    num2 = 0;
     checkAll(); //Debugging
+    //history.textContent += ' =';
     operator = '';
+    calculationComplete = true;
     return;
   } else {
     operator = button.textContent;
-    //display.textContent = operator;
+    //mainDisplay.textContent = operator;
     num2 = 0;
+
     console.log(`${num1} ${operator}`);
 
     clearOnNextInput = true;
+  }
+});
+
+//Keyboard functionality
+window.addEventListener('keydown', (e) => {
+  let key = e.code;
+
+  if (e.shiftKey) {
+    if (e.code === 'Equal') key = 'NumpadAdd';
+    if (e.code === 'Digit8') key = 'NumpadMultiply';
+  }
+
+  const button = document.querySelector(`button[data-key="${key}"]`);
+
+  if (!button) return;
+  if (e.code === 'Enter') {
+    e.preventDefault();
+  }
+
+  if (button) button.click(); // Simulate a button click
+});
+
+window.addEventListener('keydown', (e) => {
+  if (e.key === 'Backspace') {
+    console.log(e.key);
+    if (!num1 && !operator) {
+      if (mainDisplay.textContent > 0 && clearOnNextInput === false) {
+        mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+        history.textContent = mainDisplay.textContent.slice(0, -1);
+      } else {
+        return;
+      }
+    } else {
+      mainDisplay.textContent = mainDisplay.textContent.slice(0, -1);
+    }
   }
 });
